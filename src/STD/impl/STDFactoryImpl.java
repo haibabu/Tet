732803be55ef120053eea85b56f02804fd9f8f2f
PROJECT_2 <PROJECT_2@Babu-HP>
2017-02-02 21:40:29 +0530

    test

---
+++ src/STD/impl/STDFactoryImpl.java
@@ -0,0 +1,95 @@
+/**
+ */
+package STD.impl;
+
+import STD.*;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class STDFactoryImpl extends EFactoryImpl implements STDFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static STDFactory init() {
+		try {
+			STDFactory theSTDFactory = (STDFactory)EPackage.Registry.INSTANCE.getEFactory(STDPackage.eNS_URI);
+			if (theSTDFactory != null) {
+				return theSTDFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new STDFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public STDFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case STDPackage.STUDENT: return createStudent();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Student createStudent() {
+		StudentImpl student = new StudentImpl();
+		return student;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public STDPackage getSTDPackage() {
+		return (STDPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static STDPackage getPackage() {
+		return STDPackage.eINSTANCE;
+	}
+
+} //STDFactoryImpl
